6872d9ace8e9547791235049d57be46cae6423d9
Kaelan Wan <138369005+kaenugget@users.noreply.github.com>
2024-06-04 14:22:53 +0800

    feat: added Flask endpoints

--- app.py
+++ app.py
@@ -1,5 +1,33 @@
+from flask import Flask, request
 from langchain_community.llms import Ollama
 
-llm = Ollama(model="llama3")
+app = Flask(__name__)
 
-llm.invoke("Tell me a cat joke")
+
+# llm = Ollama(model="llama3")
+
+# response = llm.invoke("Tell me a cat joke")
+
+# print(response)
+
+
+@app.route("/ai", methods=["POST"])
+def aiPost():
+    print("Post /ai called")
+    json_content = request.json
+    query = json_content.get("query")
+
+    print(f"query: {query}")
+
+
+    response_answer = "Sample Response. Query: " + query
+    return response_answer
+
+
+
+
+def start_app():
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
+if __name__ == "__main__":
+    start_app()
