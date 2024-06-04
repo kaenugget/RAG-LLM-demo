6b10772db222f39e32750a1ed6d99a99e182fc94
Kaelan Wan <138369005+kaenugget@users.noreply.github.com>
2024-06-04 15:58:36 +0800

    feat: Added querying on pdf file

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain_community.document_loaders import PDFPlumberLoader 
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain.prompts import PromptTemplate
 
 
 app = Flask(__name__)
@@ -18,6 +21,14 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1024, chunk_overlap=80, length_function=len, is_separator_regex=False
 )
 
+raw_prompt = PromptTemplate.from_template("""
+    <s>[INST] You are a technical assistant good at searching documents. If you do not have an answer from the provided information say so.[/INST]
+    [INST]{input}
+        Context: {context}
+        Answer:
+    [/INST]
+""")
+
 @app.route("/ai", methods=["POST"])
 def aiPost():
     print("Post /ai called")
@@ -31,8 +42,42 @@ def aiPost():
     response_answer = {"answer": response}
     return response_answer
 
+@app.route("/ask_pdf", methods=["POST"])
+def askPDFPost():
+    print("Post /askpdf called")
+    json_content = request.json
+    query = json_content.get("query")
+    
+    print(f"query: {query}")
 
+    print("Loading vector store")
+    vector_store = Chroma(persist_directory=folder_path, embedding_function=embedding)
 
+    print("creating chain")
+    retriever = vector_store.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={
+            "k": 20,
+            "score_threshold": 0.1,
+        },
+    )
+
+    document_chain = create_stuff_documents_chain(cached_llm,raw_prompt)
+    chain = create_retrieval_chain(retriever, document_chain)
+
+    result = chain.invoke({"input": query})
+
+    print(result)
+
+    sources = []
+    for doc in result["context"]:
+        sources.append(
+            {"source": doc.metadata["source"], "page_content": doc.page_content}
+        )
+
+    response_answer = {"answer": result["answer"], "sources": sources}
+
+    return response_answer
 
 
 
