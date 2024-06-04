3b5aaf895cba180108bf29c063ca0dbcc3f061b8
Kaelan Wan <138369005+kaenugget@users.noreply.github.com>
2024-06-04 15:04:24 +0800

    feat: Added upload pdf function

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, request
 from langchain_community.llms import Ollama
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.document_loaders import PDFPlumberLoader 
+
 
 app = Flask(__name__)
 
+folder_path="db"
 
 cached_llm = Ollama(model="llama3")
 
+embedding = FastEmbedEmbeddings()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1024, chunk_overlap=80, length_function=len, is_separator_regex=False
+)
 
 @app.route("/ai", methods=["POST"])
 def aiPost():
@@ -21,6 +32,34 @@ def aiPost():
     return response_answer
 
 
+
+
+
+
+@app.route("/pdf", methods=["POST"])
+def pdfPost():
+    file = request.files["file"]
+    file_name = file.filename
+    save_file = "pdf/" + file_name
+    file.save(save_file)
+    print(f"filename: {file_name}")
+
+    loader = PDFPlumberLoader(save_file)
+    docs = loader.load_and_split()
+    print(f"docs len={len(docs)}")
+
+    chunks = text_splitter.split_documents(docs)
+    print(f"chunks len={len(chunks)}")
+
+    #vector store into chromadb
+    vector_store = Chroma.from_documents(
+        documents=chunks, embedding=embedding, persist_directory=folder_path
+    )
+
+    vector_store.persist()
+
+    response = {"status": "Successfully Uploaded", "filename": file_name, "doc_len": len(docs), "chunks": len(chunks),}
+    return response
 
 
 def start_app():
